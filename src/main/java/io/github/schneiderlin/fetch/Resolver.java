98af59be6bfb2adcfc248bec37e4e67e3b3f9e55
林航辉 <hanghui.lin@shopcider.com>
2023-06-07 18:39:38 +0800

    fix arg type error

--- src/main/java/io/github/schneiderlin/fetch/Resolver.java
+++ src/main/java/io/github/schneiderlin/fetch/Resolver.java
@@ -22,9 +22,18 @@ public class Resolver {
                     Class clazz = Class.forName(key);
                     List<Object> ids = value.map(request -> request.request.getId()).distinct();
 
+
                     Method[] declaredMethods = clazz.getDeclaredMethods();
                     Method method = Arrays.stream(declaredMethods).filter(m -> m.getName().equals("batchQuery")).findAny().get();
-                    Map<Object, Object> map = (Map<Object, Object>) method.invoke(null, fetchContext, ids);
+                    Class<?> idsClassType = method.getParameterTypes()[1];
+
+                    Map<Object, Object> map;
+                    if (idsClassType == List.class) {
+                        map = (Map<Object, Object>) method.invoke(null, fetchContext, ids);
+                    } else {
+                        map = (Map<Object, Object>) method.invoke(null, fetchContext, ids.asJava());
+                    }
+
                     return IO
                             .sequence(value.map(request ->
                                     IORef.writeIORef(request.result, map.get(request.request.getId()).get())))
@@ -40,5 +49,15 @@ public class Resolver {
 
             return IO.parallel(ios.toList());
         };
+    }
+
+    public static void main(String[] args) {
+        Method method = Resolver.class.getMethods()[2];
+        Class<?> parameterType = method.getParameterTypes()[0];
+        System.out.println(parameterType == List.class);
+    }
+
+    public void foo(java.util.List<Integer> l) {
+
     }
 }
